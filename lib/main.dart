aade38d3f2850aff3e7d3f940ad44e8ab51c6b6d
aibrahim <ahmadji.ibr11@gmail.com>
2023-04-01 06:09:45 +0200

    finish project

--- lib/main.dart
+++ lib/main.dart
@@ -1,115 +1,169 @@
+import 'package:audioplayers/audioplayers.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(const XylophoneApp());
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+class XylophoneApp extends StatelessWidget {
+  const XylophoneApp({Key? key}) : super(key: key);
 
-  // This widget is the root of your application.
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
+  void playNote(int noteNumber) {
+    final player = AudioPlayer();
+    player.play(AssetSource('sounds/note$noteNumber.wav'));
+  }
+
+  TextButton buildKey(
+      {required Color color,
+      required int soundNumber,
+      required double height,
+      required String noteName}) {
+    return TextButton(
+      style: TextButton.styleFrom(
+        fixedSize: Size(100, height),
+        foregroundColor: Colors.white,
+        backgroundColor: color,
+        textStyle: const TextStyle(fontSize: 20),
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(20),
+        ),
+        elevation: 10,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      onPressed: () {
+        playNote(soundNumber);
+      },
+      child: Stack(
+        children: [
+          Positioned(
+            left: 30.0,
+            right: 30.0,
+            top: 10,
+            child: Container(
+              width: 40,
+              height: 40,
+              decoration: BoxDecoration(
+                shape: BoxShape.circle,
+                color: Colors.white,
+                boxShadow: [
+                  BoxShadow(
+                    color: Colors.black.withOpacity(0.2),
+                    spreadRadius: 2,
+                    blurRadius: 4,
+                    offset: const Offset(0, 3),
+                  ),
+                ],
+              ),
+            ),
+          ),
+          Positioned(
+            left: 30.0,
+            right: 30.0,
+            bottom: 10,
+            child: Container(
+              width: 35,
+              height: 35,
+              decoration: BoxDecoration(
+                shape: BoxShape.circle,
+                color: Colors.white,
+                boxShadow: [
+                  BoxShadow(
+                    color: Colors.black.withOpacity(0.2),
+                    spreadRadius: 2,
+                    blurRadius: 4,
+                    offset: const Offset(0, 3),
+                  ),
+                ],
+              ),
+            ),
+          ),
+          Center(child: Text(noteName)),
+        ],
+      ),
     );
   }
-}
-
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
-    return Scaffold(
-      appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
+    // Set landscape orientation
+    SystemChrome.setPreferredOrientations([
+      DeviceOrientation.landscapeLeft,
+      DeviceOrientation.landscapeRight,
+    ]);
+
+    return MaterialApp(
+      home: Scaffold(
+        body: Stack(children: [
+          Container(
+            decoration: const BoxDecoration(
+              image: DecorationImage(
+                image: AssetImage("assets/images/wallpaper.jpg"),
+                fit: BoxFit.cover,
+              ),
             ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
+            child: Center(
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: <Widget>[
+                  buildKey(
+                      color: Colors.red,
+                      soundNumber: 1,
+                      height: 350,
+                      noteName: 'C'),
+                  const SizedBox(width: 10),
+                  buildKey(
+                      color: Colors.orange,
+                      soundNumber: 2,
+                      height: 330,
+                      noteName: 'D'),
+                  const SizedBox(width: 10),
+                  buildKey(
+                      color: Colors.yellow,
+                      soundNumber: 3,
+                      height: 310,
+                      noteName: 'E'),
+                  const SizedBox(width: 10),
+                  buildKey(
+                      color: Colors.green,
+                      soundNumber: 4,
+                      height: 290,
+                      noteName: 'F'),
+                  const SizedBox(width: 10),
+                  buildKey(
+                      color: Colors.blue,
+                      soundNumber: 5,
+                      height: 270,
+                      noteName: 'G'),
+                  const SizedBox(width: 10),
+                  buildKey(
+                      color: Colors.indigo,
+                      soundNumber: 6,
+                      height: 250,
+                      noteName: 'A'),
+                  const SizedBox(width: 10),
+                  buildKey(
+                      color: Colors.purple,
+                      soundNumber: 7,
+                      height: 230,
+                      noteName: 'B'),
+                ],
+              ),
             ),
-          ],
-        ),
+          ),
+          const Positioned(
+            bottom: 10,
+            right: 10,
+            child: Text(
+              '© Ahmad IBRAHIM - 2023',
+              style: TextStyle(
+                fontSize: 23,
+                color: Colors.white,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+          ),
+        ]),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
 }
